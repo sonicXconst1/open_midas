a5827e55d7b4c487606b636ea7f7571f2566a3b4
Konstantin Senkevich <konstsen@gmail.com>
2021-02-22 21:29:08 +0300

    Fix order maker.

--- src/best_price_trader.rs
+++ src/best_price_trader.rs
@@ -1,6 +1,6 @@
 pub struct BestPriceTrader {
     pub pair: agnostic::trading_pair::TradingPair,
-    pub amount: f64,
+    pub part_of_base_coin_balance: f64,
 }
 
 impl BestPriceTrader {
@@ -8,12 +8,14 @@ impl BestPriceTrader {
         &self,
         merchant: std::sync::Arc<dyn agnostic::merchant::Merchant>
     ) -> Result<(), String> {
+        let accountant = merchant.accountant();
+        let base_currency = accountant.ask(self.pair.coins.base_coin()).await?;
         let sniffer = merchant.sniffer();
         let mut best_order = sniffer.the_best_order(self.pair.clone()).await?;
-        best_order.amount = self.amount;
+        best_order.amount = base_currency.amount * self.part_of_base_coin_balance;
         let trader = merchant.trader();
         let created_trade = trader.create_trade_from_order(best_order).await?;
-        log::info!("Created trade: {:#?}", created_trade);
+        log::debug!("Created trade: {:#?}", created_trade);
         Ok(())
     }
 }
