7a8fbe3e9e16b48f49fc3e4da304a2b80d395c87
Konstantin Senkevich <konstsen@gmail.com>
2021-03-20 11:40:00 +0300

    Update to new agnsotic api.

--- src/best_price_trader.rs
+++ src/best_price_trader.rs
@@ -1,24 +1,21 @@
+use agnostic::trading_pair::TradingPair;
+use agnostic::merchant::Merchant;
+use agnostic::trade::Trade;
+use std::sync::Arc;
+
 pub struct BestPriceMarketTrader {
-    pub pair: agnostic::trading_pair::TradingPair,
+    pub pair: TradingPair,
     pub amount: f64,
 }
 
 impl BestPriceMarketTrader {
-    pub async fn iterate(
-        &self,
-        merchant: std::sync::Arc<dyn agnostic::merchant::Merchant>,
-    ) -> Result<(), String> {
+    pub async fn iterate(&self, merchant: Arc<dyn Merchant>) -> Result<Trade, String> {
         let sniffer = merchant.sniffer();
-        let mut best_order = sniffer.the_best_order(self.pair.clone()).await?;
+        let mut best_order = sniffer.all_the_best_orders(self.pair.clone(), 1)
+            .await?
+            .remove(0);
         best_order.amount = self.amount;
         let trader = merchant.trader();
-        match self.pair.target {
-            agnostic::trading_pair::Target::Market => {
-                trader.create_trade_from_order(best_order).await
-            },
-            agnostic::trading_pair::Target::Limit => {
-                trader.create_order(best_order).await
-            },
-        }
+        trader.create_order(best_order).await
     }
 }
