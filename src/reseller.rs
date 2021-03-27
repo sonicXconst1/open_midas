e939bb5697afaed505fcb952018ba733d089de58
Konstantin Senkevich <konstsen@gmail.com>
2021-03-27 14:18:18 +0300

    Reseller update.

--- src/reseller.rs
+++ src/reseller.rs
@@ -1,15 +1,16 @@
 use agnostic::trade::{TradeResult, Trade};
-use agnostic::trading_pair::Side;
 use agnostic::order::OrderWithId;
 use agnostic::merchant::Merchant;
-use agnostic::trading_pair::Coins;
+use agnostic::trading_pair::{Side, Target};
+use agnostic::trading_pair::{Coins, TradingPair};
+use std::collections::HashMap;
 use std::sync::Arc;
 
 pub type Price = f64;
 pub type Amount = f64;
+pub type Storage = HashMap<Coins, Vec<Entry>>;
 
 pub struct Entry {
-    pub coins: Coins,
     pub price: Price,
     pub amount: Amount,
 }
@@ -22,8 +23,8 @@ impl Entry {
 
 pub struct Reseller {
     // It's better to use HashSet<Coins, Vec<Entry>>
-    market_buy_storage: Vec<Entry>,
-    market_sell_storage: Vec<Entry>,
+    market_buy_storage: Storage,
+    market_sell_storage: Storage,
     merchant: Arc<dyn Merchant>,
 }
 
@@ -32,7 +33,7 @@ impl Reseller {
         let coins = trade.trading_pair().coins;
         let price = trade.price();
         let amount = trade.amount();
-        let storage: &mut Vec<_> = match trade {
+        let storage: &mut Storage = match trade {
             Trade::Market(trade_result) => {
                 match trade_result.trading_pair.side {
                     Side::Sell => {
@@ -47,28 +48,56 @@ impl Reseller {
                 unimplemented!("Limit orders are not supported.")
             },
         };
-        accept_new_item(storage, coins, price, amount)
+        accept_new_item(storage, &coins, price, amount)
     }
 
-    pub async fn iterate(&self) -> Trade {
+    pub async fn iterate(&mut self) -> Result<Trade, String> {
         let accountant = self.merchant.accountant();
         let sniffer = self.merchant.sniffer();
         let trade = self.merchant.trader();
+        for (coins, entries) in self.market_sell_storage.iter_mut() {
+            let trading_pair = TradingPair {
+                coins: coins.clone(),
+                target: Target::Market,
+                side: Side::Buy,
+            };
+            let currency = accountant.ask(trading_pair.coin_to_spend()).await?;
+            let orders = sniffer.all_the_best_orders(trading_pair.clone(), 5).await?;
+            let the_best_order = match orders.get(0) {
+                Some(order) => order,
+                None => return Err(format!(
+                    "Sniffer failed to sniff orders.\nPair: {:#?}\nOrders: {:#?}",
+                    trading_pair,
+                    orders)),
+            };
+            let currency_to_spend = agnostic::price::convert_to_base_coin_amount(
+                trading_pair.target,
+                trading_pair.side,
+                &the_best_order.price.into(),
+                currency.amount);
+            // amount calculator requrired here to find a way to get valid amount.
+        }
         // process sell side first
         unimplemented!()
     }
 }
 
 fn accept_new_item(
-    storage: &mut Vec<Entry>,
-    coins: Coins,
+    storage: &mut Storage,
+    coins: &Coins,
     new_price: Price,
     new_amount: Amount) {
-    match storage.iter_mut().find(|entry| entry.coins == coins && entry.price == new_price) {
+    let entries = match storage.get_mut(coins) {
+        Some(entries) => entries,
+        None => {
+            storage.insert(coins.clone(), Vec::new());
+            storage.get_mut(coins).unwrap()
+        },
+    };
+    match entries.iter_mut().find(|entry| entry.price == new_price) {
         Some(entry) => entry.incremented(new_amount),
         None => {
-            storage.push(Entry {
-                coins, 
+            entries.push(Entry {
                 price: new_price,
                 amount: new_amount,
             })
