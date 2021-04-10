d5e1d16027ddf3f6a9c20eb37adf218a9b78985b
Konstantin Senkevich <konstsen@gmail.com>
2021-04-10 23:39:38 +0300

    Create limit master orders, Merchant Id's.

--- src/limit_master.rs
+++ src/limit_master.rs
@@ -4,13 +4,34 @@ use agnostic::trading_pair::{Coins, Side, Target, TradingPair};
 use agnostic::order::Order;
 use std::collections::HashMap;
 
+// 1. Aggregate data from merchants.
+// 2. Check state of currency orders. How?!
+
+pub type MerchantId = usize;
+
 pub struct LimitProposal {
 }
 
-#[derive(Default, Clone, Debug)]
+#[derive(Clone, Debug)]
+pub struct LimitMasterOrder {
+    pub merchant_id: MerchantId,
+    pub order: Order,
+}
+
+impl LimitMasterOrder {
+    pub fn new(merchant_id: MerchantId, order: Order) -> Self {
+        LimitMasterOrder {
+            merchant_id,
+            order,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct OrdersStorage {
-    pub market: HashMap<Side, Vec<Order>>,
-    pub limit: HashMap<Side, Vec<Order>>,
+    pub coins: Coins,
+    pub stock_market: HashMap<Side, Vec<LimitMasterOrder>>,
+    pub stock_limit: HashMap<Side, Vec<LimitMasterOrder>>,
 }
 
 #[allow(dead_code)]
@@ -20,8 +41,20 @@ pub struct LimitMaster<'a> {
 
 impl<'a> LimitMaster<'a> {
     pub async fn iterate(&self, coins: Coins) -> Result<Trade, String> {
-        let orders_storage = self.accumulate_merchants_infomration(coins).await;
+        let _orders_storage = self.accumulate_merchants_infomration(coins).await;
         unimplemented!()
+    }
+
+    pub fn get_mercahnt_id(&self, merchant: &dyn Merchant) -> Option<MerchantId> {
+        self.merchants
+            .iter()
+            .enumerate()
+            .find(|(_index, m)| std::ptr::eq(**m, merchant))
+            .map(|(index, _merchant)| index)
+    }
+
+    pub fn get_merchant(&self, id: MerchantId) -> Option<&dyn Merchant> {
+        self.merchants.get(id).map(|value| *value)
     }
 
     async fn accumulate_merchants_infomration(&self, coins: Coins) -> OrdersStorage {
@@ -40,7 +73,9 @@ impl<'a> LimitMaster<'a> {
                     sniffer.all_the_best_orders(trading_pair, 15).await
                         .unwrap()
                         .into_iter()
-                        .for_each(|order| collection.push(order));
+                        .for_each(|order| collection.push(LimitMasterOrder::new(
+                                    self.get_mercahnt_id(*merchant).unwrap(),
+                                    order)));
                 };
                 match target {
                     Target::Market => market_orders_collection.insert(*side, collection).unwrap(),
@@ -49,8 +84,9 @@ impl<'a> LimitMaster<'a> {
             };
         };
         OrdersStorage {
-            market: market_orders_collection,
-            limit: limit_orders_collection,
+            coins,
+            stock_market: market_orders_collection,
+            stock_limit: limit_orders_collection,
         }
     }
 }
