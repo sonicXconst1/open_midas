8596d55447bec754f23d7d1f0e5e6ecb8eba0337
Konstantin Senkevich <konstsen@gmail.com>
2021-04-11 22:03:01 +0300

    Aggregation methods refactoring.

--- src/limit_master.rs
+++ src/limit_master.rs
@@ -1,25 +1,25 @@
 use agnostic::merchant::Merchant;
 use agnostic::trade::Trade;
 use agnostic::trading_pair::{Coins, Side, Target, TradingPair};
-use agnostic::order::Order;
+use agnostic::order::{Order, OrderWithId};
 use std::collections::HashMap;
 
 // 1. Aggregate data from merchants.
-// 2. Check state of currency orders. How?!
+// 2. We should also check our current orders. If their volume changed - do some action: make
+// a trade or something.
+// 3. Update data aggregation.
+// 3. Check state of currency orders. How?!
 
 pub type MerchantId = usize;
 
-pub struct LimitProposal {
-}
-
 #[derive(Clone, Debug)]
-pub struct LimitMasterOrder {
+pub struct LimitMasterOrder<TOrder> {
     pub merchant_id: MerchantId,
-    pub order: Order,
+    pub order: TOrder,
 }
 
-impl LimitMasterOrder {
-    pub fn new(merchant_id: MerchantId, order: Order) -> Self {
+impl<TOrder> LimitMasterOrder<TOrder> {
+    pub fn new(merchant_id: MerchantId, order: TOrder) -> Self {
         LimitMasterOrder {
             merchant_id,
             order,
@@ -28,20 +28,21 @@ impl LimitMasterOrder {
 }
 
 #[derive(Clone, Debug)]
-pub struct OrdersStorage {
+pub struct OrdersStorage<TOrder> {
     pub coins: Coins,
-    pub stock_market: HashMap<Side, Vec<LimitMasterOrder>>,
-    pub stock_limit: HashMap<Side, Vec<LimitMasterOrder>>,
+    pub stock_market: HashMap<Side, Vec<LimitMasterOrder<TOrder>>>,
+    pub stock_limit: HashMap<Side, Vec<LimitMasterOrder<TOrder>>>,
 }
 
 #[allow(dead_code)]
 pub struct LimitMaster<'a> {
-    merchants: [&'a dyn Merchant],
+    merchants: &'a [&'a dyn Merchant],
+    my_orders_last_state: OrdersStorage<OrderWithId>,
 }
 
 impl<'a> LimitMaster<'a> {
     pub async fn iterate(&self, coins: Coins) -> Result<Trade, String> {
-        let _orders_storage = self.accumulate_merchants_infomration(coins).await;
+        let stock_orders_storage = self.accumulate_merchants_infomration(coins).await;
         unimplemented!()
     }
 
@@ -57,7 +58,31 @@ impl<'a> LimitMaster<'a> {
         self.merchants.get(id).map(|value| *value)
     }
 
-    async fn accumulate_merchants_infomration(&self, coins: Coins) -> OrdersStorage {
+    async fn accumulate_merchants_infomration(&self, coins: Coins) -> OrdersStorage<Order> {
+        self.accumulate(coins, |merchant, trading_pair| {
+            let sniffer = merchant.sniffer();
+            let future = async move {
+                sniffer.all_the_best_orders(trading_pair, 15).await.unwrap()
+            };
+            Box::pin(future)
+        }).await
+    }
+
+    async fn accumulate_my_current_order(&self, coins: Coins) -> OrdersStorage<OrderWithId> {
+        self.accumulate(coins, |merchant, trading_pair| {
+            let sniffer = merchant.sniffer();
+            let future = async move {
+                sniffer.get_my_orders(trading_pair).await.unwrap()
+            };
+            Box::pin(future)
+        }).await
+    }
+
+    async fn accumulate<TOutput: std::iter::IntoIterator>(
+        &self,
+        coins: Coins,
+        sniff_callback: impl Fn(&dyn Merchant, TradingPair) -> std::pin::Pin<Box<dyn futures::Future<Output = TOutput>>>
+    ) -> OrdersStorage<TOutput::Item> {
         let mut market_orders_collection = HashMap::new();
         let mut limit_orders_collection = HashMap::new();
         for side in &[Side::Sell, Side::Buy] {
@@ -69,9 +94,7 @@ impl<'a> LimitMaster<'a> {
                         side: side.clone(),
                         target: target.clone(),
                     };
-                    let sniffer = merchant.sniffer();
-                    sniffer.all_the_best_orders(trading_pair, 15).await
-                        .unwrap()
+                    sniff_callback(*merchant, trading_pair).await
                         .into_iter()
                         .for_each(|order| collection.push(LimitMasterOrder::new(
                                     self.get_mercahnt_id(*merchant).unwrap(),
