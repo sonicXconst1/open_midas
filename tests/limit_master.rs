450d72ee4a4f5f64d269fb96c8d8fe57970ab94d
Konstantin Senkevich <konstsen@gmail.com>
2021-05-30 00:23:47 +0300

    Limit Test Context is ready to work.

--- tests/limit_master.rs
+++ tests/limit_master.rs
@@ -5,7 +5,6 @@ use agnostic::{
     trading_pair::{Coins, Side, Target, TradingPair},
     order::OrderWithId,
     trade::Trade,
-    market::Trader,
 };
 use agnostic_test::{
     merchant::Merchant as MerchantTest,
@@ -17,67 +16,66 @@ use open_midas::{
 };
 use std::sync::Arc;
 
-pub struct LimitMasterTestContext<'a, const TRADERS_COUNT: usize> {
+pub struct LimitMasterTestContext<const TRADERS_COUNT: usize> {
     pub traders: [Arc<TradesLogger>; TRADERS_COUNT],
-    pub merchants: [&'a dyn Merchant; TRADERS_COUNT]
+    pub merchants: [Arc<dyn Merchant>; TRADERS_COUNT]
 }
 
-impl<'a, const TRADERS_COUNT: usize> LimitMasterTestContext<'a, TRADERS_COUNT> {
+impl<const TRADERS_COUNT: usize> LimitMasterTestContext<TRADERS_COUNT> {
     pub fn new(
         traders: [Arc<TradesLogger>; TRADERS_COUNT],
-        create_merchant: impl Fn(Arc<dyn Trader>) -> &'a dyn Merchant,
-    ) -> LimitMasterTestContext<'a, TRADERS_COUNT> {
-        let merchants = traders.each_ref().map(|item| create_merchant(item.clone())); 
+        merchants: [Arc<dyn Merchant>; TRADERS_COUNT],
+    ) -> LimitMasterTestContext<TRADERS_COUNT> {
         LimitMasterTestContext {
             traders,
             merchants,
         }
     }
 
-    pub fn with_default_traders(
-        create_merchant: impl Fn(Arc<dyn Trader>) -> &'a dyn Merchant,
-    ) -> Self {
-        LimitMasterTestContext::new(
-            [0; TRADERS_COUNT].map(|_| Arc::new(TradesLogger::default())),
-            create_merchant,
-        )
-    }
-
     pub fn traders_with_trade(
         trades: [Trade; TRADERS_COUNT],
-        create_merchant: impl Fn(Arc<dyn Trader>) -> &'a dyn Merchant,
     ) -> Self {
+        let traders = trades.map(|trade| Arc::new(TradesLogger::with_orders(
+                TraderTest::default(),
+                vec![trade])));
+        fn create_merchant(trader: &Arc<TradesLogger>) -> Arc<dyn Merchant> {
+            Arc::new(MerchantTest::with_trader(trader.clone()))
+        }
+        let merchants = traders.each_ref().map(create_merchant);
         LimitMasterTestContext::new(
-            trades.map(|item| Arc::new(TradesLogger::with_orders(TraderTest::default(), vec![item]))),
-            create_merchant,
+            traders,
+            merchants
         )
     }
 
-    pub fn merchants(&self) -> &[&dyn Merchant] {
-        &self.merchants
+    pub fn merchants(&self) -> [&dyn Merchant; TRADERS_COUNT] {
+        self.merchants.each_ref().map(|item| item.as_ref())
     }
-}
-
-impl<'a> LimitMasterTestContext<'a, 4> {
 }
 
 #[test]
 fn default_limit_master() {
-    let first_trader = Arc::new(TradesLogger::default());
-    let second_trader = Arc::new(TradesLogger::default());
-    let third_trader = Arc::new(TradesLogger::default());
-    let fourth_trader = Arc::new(TradesLogger::default());
-    let merchants: [&dyn Merchant; 4] = [
-        &MerchantTest::with_trader(first_trader.clone()),
-        &MerchantTest::with_trader(second_trader.clone()),
-        &MerchantTest::with_trader(third_trader.clone()),
-        &MerchantTest::with_trader(fourth_trader.clone()),
-    ];
+    let trading_pair = TradingPair {
+        coins: Coins::TonUsdt,
+        side: Side::Buy,
+        target: Target::Limit,
+    };
+    let trade_buy = Trade::Limit(OrderWithId {
+        id: "1337".into(),
+        trading_pair: trading_pair.clone(),
+        price: 100f64,
+        amount: 100f64,
+    });
+    let test_context = LimitMasterTestContext::traders_with_trade(
+        [trade_buy.clone(), trade_buy.clone(), trade_buy.clone(), trade_buy.clone()]);
+    let merchants = test_context.merchants();
     let merchants_manager = MerchantIdManager::new(&merchants);
-    let price_calculator = PriceCalculator { profit: 0.3f64 };
+    let price_calculator = PriceCalculator {
+        profit: 0.3f64,
+    };
     let amount_calculator = AmountCalculator {
         min_amount_threshold: 1f64,
-        fee: 0.01,
+        fee: 0.01f64
     };
     let mut limit_master = LimitMaster::new(
         Coins::TonUsdt,
@@ -91,12 +89,10 @@ fn default_limit_master() {
     let result = limit_master.update_orders();
     let result = tokio_test::block_on(result);
     assert!(result.is_ok());
-    println!("{:#?}", result);
 
-    assert_eq!(first_trader.create_order_log.lock().unwrap().len(), 2);
-    assert_eq!(second_trader.create_order_log.lock().unwrap().len(), 2);
-    assert_eq!(third_trader.create_order_log.lock().unwrap().len(), 2);
-    assert_eq!(fourth_trader.create_order_log.lock().unwrap().len(), 2);
+    for trader in test_context.traders.iter() {
+        assert_eq!(trader.create_order_log.lock().unwrap().len(), 3);
+    }
 }
 
 #[test]
@@ -106,16 +102,37 @@ fn initialize_traders() {
         side: Side::Sell,
         target: Target::Limit,
     };
-    let trade_sell = Trade::Limit(OrderWithId {
-        id: "1337".into(),
-        trading_pair: trading_pair.clone(),
-        price: 100f64,
-        amount: 100f64,
-    });
     let trade_buy = Trade::Limit(OrderWithId {
         id: "1337".into(),
         trading_pair: trading_pair.clone(),
         price: 100f64,
         amount: 100f64,
     });
+    let test_context = LimitMasterTestContext::traders_with_trade(
+        [trade_buy.clone(), trade_buy.clone(), trade_buy.clone(), trade_buy.clone()]);
+    let merchants = test_context.merchants();
+    let merchants_manager = MerchantIdManager::new(&merchants);
+    let price_calculator = PriceCalculator {
+        profit: 0.3f64,
+    };
+    let amount_calculator = AmountCalculator {
+        min_amount_threshold: 1f64,
+        fee: 0.01f64
+    };
+    let mut limit_master = LimitMaster::new(
+        Coins::TonUsdt,
+        merchants_manager,
+        price_calculator,
+        amount_calculator,
+    );
+    let trades = limit_master.check_current_orders();
+    let trades = tokio_test::block_on(trades);
+    assert!(trades.is_ok());
+    let result = limit_master.update_orders();
+    let result = tokio_test::block_on(result);
+    assert!(result.is_ok());
+
+    for trader in test_context.traders.iter() {
+        assert_eq!(trader.create_order_log.lock().unwrap().len(), 3);
+    }
 }
